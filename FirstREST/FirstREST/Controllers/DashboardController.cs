f7ac75de2ff8282ba3ee1e59ffc9e70bb17da62a
jpamorim <josepedropamorim@gmail.com>
2015-11-15 22:05:35 +0000

    routes para controladores, view de dashboard criada

---
+++ FirstREST/FirstREST/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace FirstREST.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
