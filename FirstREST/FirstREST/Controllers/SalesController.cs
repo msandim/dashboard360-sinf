db0dfb37bfceaebe1b191864e07e895bbb85ba26
jpamorim <josepedropamorim@gmail.com>
2015-11-16 11:00:21 +0000

    Restruturaçao para por ApiControllers dentro da pasta API para não haver conflito de controladores. Criação de controlador SalesController e view Sales/Index.cshtml

---
+++ FirstREST/FirstREST/Controllers/SalesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace FirstREST.Controllers
+{
+    public class SalesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
