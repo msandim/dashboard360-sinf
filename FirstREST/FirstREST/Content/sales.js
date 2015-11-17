fcf5eb13c26390c21ea8c58447a93594d3dcf97a
jpamorim <josepedropamorim@gmail.com>
2015-11-17 19:47:10 +0000

    Pie e line chart no sales view, falta inserir os dados atraves do ficheiro javascript ja criado (sales.js)

---
+++ FirstREST/FirstREST/Content/sales.js
@@ -0,0 +1,19 @@
+﻿function ready() {
+    create_net_sales_graph();
+    create_top_customers();
+    create_sales_by_category();
+}
+
+function create_net_sales_graph() {
+
+}
+
+function create_top_customers() {
+
+}
+
+function create_sales_by_category() {
+
+}
+
+$(document).ready(ready);
--- FirstREST/FirstREST/Controllers/DashboardController.cs
+++ FirstREST/FirstREST/Controllers/DashboardController.cs
@@ -38,3 +38,5 @@ namespace FirstREST.Controllers
     }
 
 }
+
+
