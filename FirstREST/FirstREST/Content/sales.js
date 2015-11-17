c979d81b15abe67005c866d111212723a3d47956
jpamorim <josepedropamorim@gmail.com>
2015-11-17 22:10:57 +0000

    Grafos ainda com valores falsos, falta usar os dados recolhidos da api para mostrar

--- FirstREST/FirstREST/Content/sales.js
+++ FirstREST/FirstREST/Content/sales.js
@@ -1,19 +1,127 @@
-﻿function ready() {
-    create_net_sales_graph();
-    create_top_customers();
-    create_sales_by_category();
+﻿function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
 }
 
-function create_net_sales_graph() {
+function load_sales_by_category() {
+    var today = new Date();
+    var five_years_ago = new Date(new Date().getFullYear() - 5, 0, 1)
 
+    $.ajax({
+
+        url: 'http://localhost:49822/api/sale',
+        type: 'Get',
+        data: {
+            initialDate: formatDate(five_years_ago),
+            finalDate: formatDate(today),
+            DocumentType: 'ECL'
+        },
+        success: function (data) {
+            create_sales_by_category(data);
+        },
+        failure: function () {
+            alert('Failed to get sales values');
+        }
+    });
+}
+
+function load_net_sales() {
+    var today = new Date();
+    var five_years_ago = new Date(new Date().getFullYear() - 5, 0, 1)
+
+    $.ajax({
+        url: 'http://localhost:49822/api/sale',
+        type: 'Get',
+        data: {
+            initialDate: formatDate(five_years_ago),
+            finalDate: formatDate(today),
+            DocumentType: 'ECL'
+        },
+        success: function (data) {
+            create_net_sales(data);
+        },
+        failure: function () {
+            alert('Failed to get sales values');
+        }
+    });
+}
+
+
+function ready() {
+    load_sales_by_category();
+    load_net_sales();
+    create_top_customers();
+}
+
+function create_net_sales(data) {
+    var ctx = $("#net_sales_chart").get(0).getContext("2d");
+
+    var data = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
+
+    var myLineChart = new Chart(ctx).Line(data);
 }
 
 function create_top_customers() {
 
 }
 
-function create_sales_by_category() {
+function create_sales_by_category(data) {
+    var ctx = $("#sales_by_category_chart").get(0).getContext("2d");
+    /*
+    $.each(data, function () {
+        $.each(this, function (k, v) {
+            alert(k + "  " + v);
+        });
+    });
+    */
+    var pie_data = [
+        {
+            value: data[0]["Value"]["Value"],
+            color: "yellow",
+            label: data[0]["Product"]["FamilyId"]
+        }
+        
+    ];
 
+    var options = {
+        animation: false,
+        responsive: true,
+        maintainAspectRatio: true
+    };
+
+    var piechart = new Chart(ctx).Pie(pie_data, options);
 }
 
 $(document).ready(ready);
+
