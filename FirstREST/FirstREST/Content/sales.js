3ff471933d0873cd18704057d04f720e09b6c4c5
jpamorim <josepedropamorim@gmail.com>
2015-11-18 09:41:12 +0000

    Line chart done

--- FirstREST/FirstREST/Content/sales.js
+++ FirstREST/FirstREST/Content/sales.js
@@ -32,50 +32,66 @@ function load_sales_by_category() {
     });
 }
 
-function load_net_sales() {
-    var today = new Date();
-    var five_years_ago = new Date(new Date().getFullYear() - 5, 0, 1)
 
+
+function get_load_sales(initDate, finalDate) {
+    var result = "failed";
     $.ajax({
         url: 'http://localhost:49822/api/sale',
         type: 'Get',
+        async: false,
         data: {
-            initialDate: formatDate(five_years_ago),
-            finalDate: formatDate(today),
+            initialDate: formatDate(initDate),
+            finalDate: formatDate(finalDate),
             DocumentType: 'ECL'
+
         },
         success: function (data) {
-            create_net_sales(data);
+            result = data;
         },
         failure: function () {
             alert('Failed to get sales values');
         }
     });
+    return result;
 }
 
+function add_net_sales(data) {
+    var total = 0;
 
-function ready() {
-    load_sales_by_category();
-    load_net_sales();
-    create_top_customers();
+    if (data == "failed")
+        return -1;
+
+    for (var i = 0; i < data.length; i++)
+    {
+        total += data[i]["Value"]["Value"];
+    }
+    return total;
 }
 
-function create_net_sales(data) {
+function create_net_sales() {
     var ctx = $("#net_sales_chart").get(0).getContext("2d");
 
+    var today = new Date();
+    var four_years_ago = new Date(new Date().getFullYear() - 4, 0, 1);
+    var three_years_ago = new Date(new Date().getFullYear() - 3, 0, 1);
+    var two_years_ago = new Date(new Date().getFullYear() - 2, 0, 1);
+    var one_year_ago = new Date(new Date().getFullYear() - 1, 0, 1);
+    var three_years_ago_value = add_net_sales(get_load_sales(four_years_ago, three_years_ago));
+    var two_years_ago_value = three_years_ago_value + add_net_sales(get_load_sales(three_years_ago, two_years_ago));
+    var one_year_ago_value = two_years_ago_value + add_net_sales(get_load_sales(two_years_ago, one_year_ago));
+    var today_value = one_year_ago_value + add_net_sales(get_load_sales(one_year_ago, today));
+
+    var pie_data = [];
+    pie_data[three_years_ago.getFullYear()] = three_years_ago_value;
+    pie_data[two_years_ago.getFullYear()] = two_years_ago_value;
+    pie_data[one_year_ago.getFullYear()] = one_year_ago_value;
+    pie_data[today.getFullYear()] = today_value;
+
+
     var data = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        labels: Object.keys(pie_data),
         datasets: [
-            {
-                label: "My First dataset",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [65, 59, 80, 81, 56, 55, 40]
-            },
             {
                 label: "My Second dataset",
                 fillColor: "rgba(151,187,205,0.2)",
@@ -84,12 +100,18 @@ function create_net_sales(data) {
                 pointStrokeColor: "#fff",
                 pointHighlightFill: "#fff",
                 pointHighlightStroke: "rgba(151,187,205,1)",
-                data: [28, 48, 40, 19, 86, 27, 90]
+                data: [three_years_ago_value, two_years_ago_value, one_year_ago_value, today_value]
             }
         ]
     };
 
-    var myLineChart = new Chart(ctx).Line(data);
+    var options = {
+        animation: false,
+        responsive: true,
+        maintainAspectRatio: true
+    };
+
+    var myLineChart = new Chart(ctx).Line(data, options);
 }
 
 function create_top_customers() {
@@ -125,3 +147,8 @@ function create_sales_by_category(data) {
 
 $(document).ready(ready);
 
+function ready() {
+    load_sales_by_category();
+    create_net_sales();
+    create_top_customers();
+}
