1b11f6a24e60b85fe01a6a8cd80c57880d908407
jpmmaia <jpmmaia@gmail.com>
2015-11-27 10:14:48 +0000

    Formatting currency values in the Sales view. Adding ColorGenerator.js

---
+++ FirstREST/FirstREST/Content/Core/Utils/ColorGenerator.js
@@ -0,0 +1,15 @@
+﻿function ColorGenerator() {
+    ColorGenerator.baseHue = 0;
+}
+
+ColorGenerator.generateChars = function (numColors)
+{
+    var colors = [];
+    for (var i = 0; i < numColors; i++)
+    {
+        var hue = Math.round(ColorGenerator.baseHue + (240 / numColors) * i) % 240;
+        colors.push(hue);
+    }
+
+    return colors;
+};
