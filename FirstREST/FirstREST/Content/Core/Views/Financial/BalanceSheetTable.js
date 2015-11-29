e6fdb2fdd50a1f4fbba99a02460d153faa0ab822
jpamorim <josepedropamorim@gmail.com>
2015-11-29 15:25:22 +0000

    BalanceSheetTabel inicial

---
+++ FirstREST/FirstREST/Content/Core/Views/Financial/BalanceSheetTable.js
@@ -0,0 +1,34 @@
+﻿function BalanceSheetTable()
+{
+}
+
+BalanceSheetTable.initialize = function (tableId, data)
+{
+    this.tableId = tableId;
+
+    this.total_non_current_assets = data.metrics.total_non_current_assets.years_data;
+    this.total_current_assets = data.metrics.total_current_assets.years_data;
+    this.total_assets = data.metrics.total_assets.years_data;
+    this.total_current_liabilities = data.metrics.total_current_liabilities.years_data;
+    this.total_long_term_debt = data.metrics.total_long_term_debt.years_data;
+    this.total_liabilities = data.metrics.total_liabilities.years_data;
+
+    this.table = new Table();
+    this.table.initialize();
+};
+
+BalanceSheetTable.displayTable = function ()
+{
+    this.table.addColumnLabel("Client ID");
+    this.table.addColumnLabel("Client Name");
+    this.table.addColumnLabel("Value");
+
+    /*
+    for (var i = 0; i < data.length; i++)
+        table.addRow(data[i].ClientId, data[i].ClientName, CurrencyUtils.format(data[i].Total) + " €");
+    */
+
+   
+
+    this.table.display(this.tableId);
+};
--- FirstREST/FirstREST/Content/financial.js
+++ FirstREST/FirstREST/Content/financial.js
@@ -5,33 +5,46 @@ function drawCashChart(data) {
     CashChart.displayChart("#cash_chart", years_data);
 }
 
-function drawAcountsChart(data) {
+function drawAccountsChart(data) {
     var accounts_receivable = data.metrics.accounts_receivable.years_data;
     var accounts_payable = data.metrics.accounts_payable.years_data;
     AccountsChart.displayChart("#accounts_chart", "#accounts_legend", accounts_receivable, accounts_payable);
     AccountsChart.displayLegends();
 }
 
+function drawBalanceSheetTable(data) {
+    BalanceSheetTable.initialize("#balance_sheet_table", data);
+    BalanceSheetTable.displayTable();
+}
+
 
 function drawCharts(data) {
-    //console.log(data);
+    console.log(data);
     drawCashChart(data);
-    drawAcountsChart(data);
+    drawAccountsChart(data);
+    drawBalanceSheetTable(data);
 }
 
 function getBalanceSheet() {
     $.ajax({
         url: 'http://localhost:49822/api/financial',
         type: 'Get',
+        beforeSend: function () {
+            $("#cash_chart").closest("div .box").append("<div class=\"overlay\"><i class=\"fa fa-refresh fa-spin\"></i></div>");
+            $("#accounts_chart").closest("div .box").append("<div class=\"overlay\"><i class=\"fa fa-refresh fa-spin\"></i></div>");
+            $("#balance_sheet_table").closest("div .box").append("<div class=\"overlay\"><i class=\"fa fa-refresh fa-spin\"></i></div>");
+        },
         success: function (data) {
             drawCharts(data);
+            $("#cash_chart").closest("div .box").children("div .overlay").remove();
+            $("#accounts_chart").closest("div .box").children("div .overlay").remove();
+            $("#balance_sheet_table").closest("div .box").children("div .overlay").remove();
         },
         failure: function () {
             alert('Failed to get financial values');
         }
     });
 }
-
 
 function ready() {
     getBalanceSheet();
