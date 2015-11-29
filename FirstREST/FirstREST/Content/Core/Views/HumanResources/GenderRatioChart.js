5e5884b5d44c4050d6c276b86c91bf120acb075c
msandim <migsandim@hotmail.com>
2015-11-29 02:51:09 +0000

    Front-end feito, falta só descomentar o pie chart dos ratios (que estava a lançar excepção)

--- FirstREST/FirstREST/Content/Core/Views/HumanResources/GenderRatioChart.js
+++ FirstREST/FirstREST/Content/Core/Views/HumanResources/GenderRatioChart.js
@@ -1,1 +1,46 @@
-﻿
+﻿function GenderRatioChart() {
+}
+
+GenderRatioChart.display = function (canvasId, legendsId, initialDate, finalDate) {
+    $.ajax(
+        {
+            url: 'http://localhost:49822/api/humanResources/gender_count',
+            type: 'Get',
+            data: {
+                initialDate: DateUtils.formatDate(initialDate),
+                finalDate: DateUtils.formatDate(finalDate),
+            },
+            beforeSend: function () {
+                $(canvasId).closest("div .box").append("<div class=\"overlay\"><i class=\"fa fa-refresh fa-spin\"></i></div>");
+            },
+            success: function (data) {
+                GenderRatioChart.displayChart(canvasId, legendsId, data);
+                $(canvasId).closest("div .box").children("div .overlay").remove();
+            },
+            failure: function () {
+                alert('Failed to get gender values');
+            }
+        }
+    );
+};
+GenderRatioChart.displayChart = function (canvasId, legendsId, data) {
+
+    if (!GenderRatioChart.chart) {
+        GenderRatioChart.chart = new PieChart();
+    }
+    else {
+        GenderRatioChart.chart.shutdown();
+    }
+
+    GenderRatioChart.chart.initialize(canvasId, legendsId);
+
+    // Add sections:
+    for (var i = 0; i < data.length; i++) {
+        var element = data[i];
+
+        GenderRatioChart.chart.addSection(element.FamilyDescription, element.Total.toFixed(2));
+    }
+
+    // Display:
+    GenderRatioChart.chart.display();
+};
--- FirstREST/FirstREST/Content/humanResources.js
+++ FirstREST/FirstREST/Content/humanResources.js
@@ -11,9 +11,16 @@ function drawDateSelection() {
 }
 
 function drawCharts(initialDate, finalDate) {
+    EmployeesCountChart.display("#employees_count_chart", initialDate, finalDate, "month");
+    TopAbsencesChart.display("#top_absences", initialDate, finalDate, 10);
+    TopOvertimeHoursChart.display("#top_overtime_hours", initialDate, finalDate, 10);
+    //GenderRatioChart.display("#gender_ratio_chart", "gender_ratio_legend", initialDate, finalDate);
+
+    /*
     PurchasesByCategoryChart.display("#purchases_by_category_chart", "#purchases_by_category_legend", initialDate, finalDate, 5);
     TopSuppliersTable.display("#top_suppliers", initialDate, finalDate, 10);
     NetPurchasesChart.display("#net_purchases_chart", initialDate, finalDate, "month");
+    */
 }
 
 function ready() {
