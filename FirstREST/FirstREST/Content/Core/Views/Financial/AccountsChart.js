c5f9a00fdfa022d629cecc8eb9597c5968b64060
jpamorim <josepedropamorim@gmail.com>
2015-11-29 14:27:25 +0000

    Accounts Payable vs Accounts Receivable num Line chart com drill down/drill up e legendas

---
+++ FirstREST/FirstREST/Content/Core/Views/Financial/AccountsChart.js
@@ -0,0 +1,101 @@
+﻿function AccountsChart() {
+}
+
+AccountsChart.displayChart = function (canvasId, legendsId, accounts_receivable, accounts_payable) {
+    this.canvasId = canvasId;
+    this.accounts_receivable = accounts_receivable;
+    this.accounts_payable = accounts_payable;
+    this.legendsId = legendsId;
+
+    if (!AccountsChart.chart) {
+        AccountsChart.chart = new LineChart();
+    } else {
+        AccountsChart.chart.shutdown();
+    }
+
+    AccountsChart.chart.initialize();
+
+    //Accounts Receivable
+    for (var i = 0; i < accounts_receivable.length; i++) {
+        var element = accounts_receivable[i];
+        AccountsChart.chart.addValue(element.year, element.total, 0);
+    }
+
+    //Accounts Payable
+    for (var i = 0; i < accounts_payable.length; i++) {
+        var element = accounts_payable[i];
+        AccountsChart.chart.addValue(element.year, element.total, 1);
+    }
+
+    AccountsChart.chart.display(canvasId);
+
+    $(canvasId).get(0).onclick = function (event) {
+        var activePoints = AccountsChart.chart.chart.getPointsAtEvent(event);
+        if (activePoints[0] != null) {
+            var year = activePoints[0].label;
+            AccountsChart.onDrillDown(year);
+        }
+    }
+};
+
+AccountsChart.onDrillDown = function (year) {
+    AccountsChart.chart.shutdown();
+    AccountsChart.chart.initialize();
+
+    for (var i = 0; i < this.accounts_receivable.length; i++) {
+        var element_receivable = this.accounts_receivable[i];
+        if (element_receivable.year == year) {
+            var element_payable = this.accounts_payable[i];
+            for (var j = 0; j < element_receivable.months.length; j++) {
+                var label = DateUtils.formatLabelMonth(year, j);
+                var month_receivable = element_receivable.months[j];
+                var month_payable = element_payable.months[j];
+
+                AccountsChart.chart.addValue(label, month_receivable, 0);
+                AccountsChart.chart.addValue(label, month_payable, 1);
+            }
+
+            break;
+        }
+    }
+
+    //Drill Up
+    $(this.canvasId).get(0).onclick = function (event) {
+        AccountsChart.onDrillUp();
+    }
+
+    AccountsChart.chart.display(this.canvasId);
+}
+
+AccountsChart.onDrillUp = function () {
+    AccountsChart.chart.shutdown();
+    AccountsChart.chart.initialize();
+
+    //Accounts Receivable
+    for (var i = 0; i < this.accounts_receivable.length; i++) {
+        var element = this.accounts_receivable[i];
+        AccountsChart.chart.addValue(element.year, element.total, 0);
+    }
+
+    //Accounts Payable
+    for (var i = 0; i < this.accounts_payable.length; i++) {
+        var element = this.accounts_payable[i];
+        AccountsChart.chart.addValue(element.year, element.total, 1);
+    }
+
+
+    $(this.canvasId).get(0).onclick = function (event) {
+        var activePoints = AccountsChart.chart.chart.getPointsAtEvent(event);
+        if (activePoints[0] != null) {
+            var year = activePoints[0].label;
+            AccountsChart.onDrillDown(year);
+        }
+    }
+
+    AccountsChart.chart.display(this.canvasId);
+}
+
+AccountsChart.displayLegends = function () {
+    $(this.legendsId).append("<li><i class=\"fa fa-square-o\"></i> Accounts Receivable</li>");
+    $(this.legendsId).append("<li><i class=\"fa fa-square-o\"></i> Accounts Payable</li>");
+};
--- FirstREST/FirstREST/Content/financial.js
+++ FirstREST/FirstREST/Content/financial.js
@@ -5,9 +5,18 @@ function drawCashChart(data) {
     CashChart.displayChart("#cash_chart", years_data);
 }
 
+function drawAcountsChart(data) {
+    var accounts_receivable = data.metrics.accounts_receivable.years_data;
+    var accounts_payable = data.metrics.accounts_payable.years_data;
+    AccountsChart.displayChart("#accounts_chart", "#accounts_legend", accounts_receivable, accounts_payable);
+    AccountsChart.displayLegends();
+}
+
+
 function drawCharts(data) {
     //console.log(data);
     drawCashChart(data);
+    drawAcountsChart(data);
 }
 
 function getBalanceSheet() {
