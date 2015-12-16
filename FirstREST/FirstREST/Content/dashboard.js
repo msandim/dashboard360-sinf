eedbecbdb153f1bab85156630c86dc9dc1e8e31e
jpmmaia <jpmmaia@gmail.com>
2015-12-16 00:36:44 +0000

    Adding ajax to the dashboard. Changing boxes.

--- FirstREST/FirstREST/Content/dashboard.js
+++ FirstREST/FirstREST/Content/dashboard.js
@@ -1,5 +1,70 @@
-﻿function ready() {
-   
+﻿$(document).ready(ready);
+
+function setButtonValues(data)
+{
+    $("#payables_value").text(CurrencyUtils.format(data.Payables, data.Currency));
+    $("#receivables_value").text(CurrencyUtils.format(data.Receivables, data.Currency));
+    $("#gross_purchases_value").text(CurrencyUtils.format(data.GrossPurchases, data.Currency));
+    $("#gross_sales_value").text(CurrencyUtils.format(data.GrossSales, data.Currency));
+    $("#net_purchases_value").text(CurrencyUtils.format(data.NetPurchases, data.Currency));
+    $("#net_sales_value").text(CurrencyUtils.format(data.NetSales, data.Currency));
+    $("#human_resources_value").text(CurrencyUtils.format(data.LaborCostValue, data.Currency));
 }
 
-$(document).ready(ready);
+function addRefreshSpin(canvasIds)
+{
+    // TODO
+    for (var i = 0; i < canvasIds.length; i++)
+    {
+        $("#" + canvasIds[i]).closest("div .box").append("<div class=\"overlay\"><i class=\"fa fa-refresh fa-spin\"></i></div>");
+    }
+}
+
+function removeRefreshSpin(canvasIds)
+{
+    // TODO
+    for (var i = 0; i < canvasIds.length; i++)
+    {
+        $("#" + canvasIds[i]).closest("div .small-box").children("div .overlay").remove();
+    }
+}
+
+function getButtonValuesAsync(initialDate, finalDate)
+{
+    var canvasIds = [
+    "payables_value", "receivables_value", "gross_purchases_value", "gross_sales_value",
+    "net_purchases_value", "net_sales_value", "human_resources_value"
+    ];
+
+    $.ajax(
+        {
+            url: 'http://localhost:49822/api/dashboard/button_values',
+            type: 'Get',
+            data: {
+                initialDate: DateUtils.formatDate(initialDate),
+                finalDate: DateUtils.formatDate(finalDate)
+            },
+            beforeSend: function ()
+            {
+                addRefreshSpin(canvasIds);
+            },
+            success: function (data) {
+                setButtonValues(data);
+                removeRefreshSpin(canvasIds);
+            },
+            failure: function ()
+            {
+                alert('Failed to get button values');
+            }
+        }
+    );
+}
+
+function ready()
+{
+
+    var initialDate = moment().subtract(1, 'month').startOf('month');
+    var finalDate = moment().subtract(1, 'month').endOf('month');
+
+    getButtonValuesAsync(initialDate, finalDate);
+}
--- FirstREST/FirstREST/Controllers/DashboardController.cs
+++ FirstREST/FirstREST/Controllers/DashboardController.cs
@@ -1,47 +1,11 @@
-﻿using System;
-using System.Web.Mvc;
-
-using System.Globalization;
-using Dashboard.Models.Utils;
+﻿using System.Web.Mvc;
 
 namespace Dashboard.Controllers
 {
-    using Models;
-
     public class DashboardController : Controller
     {
         public ActionResult Index()
         {
-            // Get last month's first and last day
-            var today = DateTime.Today;
-            var month = new DateTime(today.Year, today.Month, 1);
-            var first = month.AddMonths(-1);
-            var last = month.AddDays(-1);
-
-            // Store the month we're seeing:
-            ViewBag.currentDate = month.AddMonths(-1).ToString("MMMM", new CultureInfo("en-US")) + " " + today.Year;
-
-            //Get payables
-            ViewBag.PayablesValue = FormatUtils.FormatCurrency(FinancialManager.GetPayables(first, last));
-
-            //Get receivables
-            ViewBag.ReceivablesValue = FormatUtils.FormatCurrency(FinancialManager.GetReceivables(first, last));
-
-            //Get Net Purchases Global Value
-            ViewBag.NetPurchasesValue = FormatUtils.FormatCurrency(PurchasesManager.GetNetPurchases(first, last));
-
-            //Get Net Sales Global Value
-            ViewBag.NetSalesValue = FormatUtils.FormatCurrency(SalesManager.GetNetSales(first, last));
-
-            //Get Gross Purchases Global Value
-            ViewBag.GrossPurchasesValue = FormatUtils.FormatCurrency(PurchasesManager.GetGrossPurchases(first, last));
-
-            //Get Gross Sales Global Value
-            ViewBag.GrossSalesValue = FormatUtils.FormatCurrency(SalesManager.GetGrossSales(first, last));
-
-            //Get Labor Cost per Employee
-            ViewBag.LaborCostValue = FormatUtils.FormatCurrency(HumanResourcesManager.GetHumanResourcesSpendings(first, last));
-
             return View();
         }
     }
